811332b1df77d46c1faea94ff2a855e6f49f263b
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-07-31 12:16:34 +0300

    update algos

--- CodeWars/6kyu/floating-point_approximation_(I).go
+++ CodeWars/6kyu/floating-point_approximation_(I).go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"math"
 )
 
 func main() {
-	res := (math.Sqrt(1+1e-15*2) - 1) / 2
-	fmt.Println(res)
+
+}
+
+func F(x float64) float64 {
+	return x / (1.0 + math.Sqrt(1.0 + x))
 }

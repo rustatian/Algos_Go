ad88b4c533620a2ee88666683260aaeb095e792a
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-10-17 22:18:54 +0300

    BS iterative

--- Algoexpert/easy/binary_search.go
+++ Algoexpert/easy/binary_search.go
@@ -1,20 +1,38 @@
 package main
 
 func BinarySearch(array []int, target int) int {
-	return binarySearchHelper(array, target, 0, len(array)-1)
+	return binarySearchHelperIterative(array, target, 0, len(array)-1)
 }
 
 func binarySearchHelper(array []int, target int, left, right int) int {
-	middle := (left + right) / 2
-	num := array[middle]
 	if left > right {
 		return -1
 	}
+	middle := (left + right) / 2
+	num := array[middle]
+
 	if num == target {
 		return middle
 	} else if num > target {
-		return binarySearchHelper(array, target, left, middle - 1)
+		return binarySearchHelper(array, target, left, middle-1)
 	} else {
-		return binarySearchHelper(array, target, middle + 1, right)
+		return binarySearchHelper(array, target, middle+1, right)
 	}
+}
+
+func binarySearchHelperIterative(array []int, target int, left, right int) int {
+	res := -1
+	for ; left <= right; {
+		middle := (left + right) / 2
+		num := array[middle]
+		if num == target {
+			return middle
+		} else if num > target {
+			right = middle - 1
+		} else {
+			left = middle + 1
+		}
+	}
+
+	return res
 }

b4417d8ecaf31e4eda36ad1e852047b284387f04
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-01 18:02:44 +0300

    move_element_to_end solved

--- Algoexpert/medium/medium_test.go
+++ Algoexpert/medium/medium_test.go
@@ -17,3 +17,11 @@ func TestSmallestDifference(t *testing.T) {
 	output := SmallestDifference([]int{-1, 5, 10, 20, 28, 3}, []int{26, 134, 135, 15, 17})
 	require.Equal(t, expected, output)
 }
+
+func TestMoveElementToEnd(t *testing.T) {
+	array := []int{5, 1, 2, 5, 5, 3, 4, 6, 7, 5, 8, 9, 10, 11, 5, 5, 12}
+	toMove := 5
+	expected := []int{12, 1, 2, 11, 10, 3, 4, 6, 7, 9, 8, 5, 5, 5, 5, 5, 5}
+	output := MoveElementToEnd(array, toMove)
+	require.Equal(t, expected, output)
+}
---
+++ Algoexpert/medium/move_element_to_end.go
@@ -0,0 +1,29 @@
+package medium
+
+// 2, 1, 2, 2, 2, 3, 4, 2 --> 1, 3, 4, 2, 2, 2, 2, 2
+//
+func MoveElementToEnd(array []int, toMove int) []int {
+	right := len(array) - 1
+	left := 0
+
+	for ; left <= right; {
+		// right element is equal toMove, continue search
+		if array[right] == toMove {
+			right--
+			continue
+		}
+		// left element is not toMove, searching when right will not be toMove, and left will be toMove to swap
+		if array[left] != toMove {
+			left++
+			continue
+		}
+
+		// at this point we know, that array[right] is not toMove and array[left] is toMove and we swap it and increment both indexes
+		tmp := array[right]
+		array[right] = toMove
+		array[left] = tmp
+		right--
+		left++
+	}
+	return array
+}

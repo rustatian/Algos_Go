57d5461799bcfc107a3b851837624bd80acb7436
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-19 12:31:51 +0300

    spiral_traverse solved

--- Algoexpert/medium/medium_test.go
+++ Algoexpert/medium/medium_test.go
@@ -35,11 +35,10 @@ func TestIsMonotonic(t *testing.T) {
 func TestSpiralTraverse(t *testing.T) {
 	matrix := [][]int{
 		{1, 2, 3, 4},
-		{12, 13, 14, 5},
-		{11, 16, 15, 6},
-		{10, 9, 8, 7},
+		{10, 11, 12, 5},
+		{9, 8, 7, 6},
 	}
-	expected := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
+	expected := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
 	actual := SpiralTraverse(matrix)
 	require.Equal(t, expected, actual)
 }
--- Algoexpert/medium/spiral_traverse.go
+++ Algoexpert/medium/spiral_traverse.go
@@ -3,20 +3,36 @@ package medium
 func SpiralTraverse(array [][]int) []int {
 	res := make([]int, 0, 0)
 	startRow := 0
+	endRow := len(array) - 1
 	startColumn := 0
-	endRow := len(array)
 	endColumn := len(array[0]) - 1
 
-	for endRow != 0 && endColumn != 0 {
-		res = append(res, array[startRow][startColumn])
-		startColumn++
-		if startColumn == endColumn {
-			for startRow != endRow {
-				res = append(res, array[startRow][startColumn])
-				startRow++
-			}
-
+	for ; startRow <= endRow && startColumn <= endColumn; {
+		for i := startColumn; i <= endColumn; i++ {
+			res = append(res, array[startRow][i])
 		}
+		for i := startRow + 1; i <= endRow; i++ {
+			res = append(res, array[i][endColumn])
+		}
+
+		for i := endColumn - 1; i >= startColumn; i-- {
+			if startRow == endRow {
+				break
+			}
+			res = append(res, array[endRow][i])
+		}
+		for i := endRow - 1; i > startRow; i-- {
+			if startColumn == endColumn {
+				break
+			}
+			res = append(res, array[i][startColumn])
+		}
+
+		startColumn += 1
+		endColumn -= 1
+		startRow += 1
+		endRow -= 1
 	}
+
 	return res
 }

58d85c6a2b3e54ce12dae5435b4fe88f4d2c9f94
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-12-12 23:09:38 +0300

    longest peak solved

--- Algoexpert/medium/longest_peak.go
+++ Algoexpert/medium/longest_peak.go
@@ -1,14 +1,30 @@
 package medium
 
 func LongestPeak(array []int) int {
-	for i := 1; i < len(array); i++ {
+	curPeak := 0
+	for i := 1; i < len(array)-1; i++ {
 		peak := array[i-1] < array[i] && array[i] > array[i+1]
 		if !peak {
 			continue
 		}
 
-		left := i
+		left := i - 2
+		for j := left; j >= 0 && array[j] < array[j+1]; {
+			j--
+			left = j
+		}
 
+		right := i + 2
+		for k := right; k < len(array) && array[k] < array[k-1]; {
+			k++
+			right = k
+		}
+
+		length := right - left - 1
+		if length > curPeak {
+			curPeak = length
+		}
+		i = right - 1
 	}
-	return -1
+	return curPeak
 }

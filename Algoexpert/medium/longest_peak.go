e9f19fa244434904dd2dde487d8cba7e03203108
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-12-12 22:00:29 +0300

    ispeak

--- Algoexpert/medium/longest_peak.go
+++ Algoexpert/medium/longest_peak.go
@@ -2,7 +2,13 @@ package medium
 
 func LongestPeak(array []int) int {
 	for i := 1; i < len(array); i++ {
-		peak := array[i]
+		peak := array[i-1] < array[i] && array[i] > array[i+1]
+		if !peak {
+			continue
+		}
+
+		left := i
+
 	}
 	return -1
 }

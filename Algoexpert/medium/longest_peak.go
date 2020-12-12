9c3f36fcb020655e7aa0e0b032313c8ec058c441
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-12-12 21:57:44 +0300

    update go

---
+++ Algoexpert/medium/longest_peak.go
@@ -0,0 +1,8 @@
+package medium
+
+func LongestPeak(array []int) int {
+	for i := 1; i < len(array); i++ {
+		peak := array[i]
+	}
+	return -1
+}
--- Algoexpert/medium/medium_test.go
+++ Algoexpert/medium/medium_test.go
@@ -42,3 +42,9 @@ func TestSpiralTraverse(t *testing.T) {
 	actual := SpiralTraverse(matrix)
 	require.Equal(t, expected, actual)
 }
+
+func TestLongestPeak(t *testing.T) {
+	array := []int{1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3}
+	actual := LongestPeak(array)
+	require.Equal(t, 6, actual)
+}

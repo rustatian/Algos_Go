e8f97c718f0859ec1f4df357fc7200e305f6a714
Valery Piashchynski <piashchynski_valery@hotmail.com>
2021-01-11 22:15:03 +0300

    first_duplicate_value naive solution with map

--- Algoexpert/medium/first_duplicate_value.go
+++ Algoexpert/medium/first_duplicate_value.go
@@ -1,6 +1,12 @@
 package medium
 
 func FirstDuplicateValue(array []int) int {
-	// Write your code here.
+	m := make(map[int]int)
+	for i := 0; i < len(array); i++ {
+		if _, ok := m[array[i]]; ok {
+			return array[i]
+		}
+		m[array[i]] = 1
+	}
 	return -1
 }
